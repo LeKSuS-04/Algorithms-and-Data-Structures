4242b74cc77ad87a0c382ddde9315a128bb57d4f
LeKSuS-04 <tarasovion2004@gmail.com>
2021-08-19 07:41:58 +0300

    Initial Commit & UFDS

---
+++ README.md
@@ -0,0 +1,9 @@
+# Algorithms & Data Structures
+Implementation of different known algorithms and data structures in C++
+
+## Disclaimer
+By the time, this repository will grow, recieving more entries. Keep in mind that all examples given here are just standart implementations of those algorithms, which, probably, need some tweaks to be used in your specific task
+
+## Resources
+Of course, I haven't figured this ideas and implementations myself; Here are resources I've learned this information from:
+* Steven Halim & Felix Halim - Competitive Programming 3: The New Lower Bound of Programming Contests ([link](https://cpbook.net/))
---
+++ data_structures/ufds.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class UFDS {
+private:
+    int n;
+    vector<int> p, rank, s;
+public:
+    UFDS(int size) {
+        p.assign(size, 0); for (int i = 0; i < size; i++) p[i] = i;
+        rank.assign(size, 0);
+        s.assign(size, 1);
+        n = size;
+    }
+
+    int find_set(int i) { return (p[i] == i ? i : (p[i] = find_set(i))); }
+    bool is_same_set(int i, int j) {  return find_set(i) == find_set(j); }
+    void union_sets(int i, int j) {
+        if (!is_same_set(i, j)) {
+            int x = find_set(i), y = find_set(j);
+            if (rank[x] > rank[y]) swap(x, y);
+            if (rank[x] == rank[y]) ++rank[y];
+            p[x] = y;
+            s[y] += s[x];
+            --n;
+        }
+    }
+    int set_size(int i) { return s[find_set(i)]; }
+    int count() { return n; }
+};
