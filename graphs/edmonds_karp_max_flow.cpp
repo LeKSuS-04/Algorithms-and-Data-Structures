fa167d4e46f8d6bddd8e84abf33c244fb770f38a
LeKSuS-04 <tarasovion2004@gmail.com>
2021-09-09 18:01:03 +0300

    bugfix in Edmonds-Karp algorithm

--- graphs/edmonds_karp_max_flow.cpp
+++ graphs/edmonds_karp_max_flow.cpp
@@ -7,7 +7,7 @@ const int INF = 1e9;
 const int MAXSIZE = 110; // Maximum amount of vertexes in graph
                          // (must be known vefore compilation);
 int n,                   // Actual amount of vertexes in graph;
-    max_flow, f,
+    max_flow,            // This gonna store the answer
     s,                   // Source: vertex to start from;
     t;                   // Destination: vertex to finish in;
 vector<vector<int>> AM;  // Adjacency matrix, where;
@@ -19,20 +19,18 @@ vector<int> p;
 bitset<MAXSIZE> visited;
 
 
-void augment(int v, int bottleneck) {
-    if (v == s) { f = bottleneck; }
+int augment(int v, int bottleneck) {
+    if (v == s) return bottleneck;
     else {
-        augment(p[v], min(bottleneck, AM[p[v]][v]));
-        AM[p[v]][v] -= f;
-        AM[v][p[v]] += f;
+        int bottleneck = augment(p[v], min(bottleneck, AM[p[v]][v]));
+        AM[p[v]][v] -= bottleneck;
+        AM[v][p[v]] += bottleneck;
     }
 }
 
 int edmonds_karp_max_flow() {
-    max_flow = 0, f = 42;
-    while (f != 0) {
-        f = 0;
-
+    max_flow = 0;
+    while (true) {
         queue<int> q; q.push(s);
         visited.reset(); visited[s] = true;
         p.assign(n, -1);
@@ -48,8 +46,8 @@ int edmonds_karp_max_flow() {
             }
         }
 
-        augment(t, INF);
-        max_flow += f;
+        if (p[t] == -1) break;
+        max_flow += augment(t, INF);
     }
     return max_flow;
 }
