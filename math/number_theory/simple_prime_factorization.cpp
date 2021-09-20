3ef6d05b309112c74e8e7f86c1397bc73dbccb8b
LeKSuS-04 <tarasovion2004@gmail.com>
2021-09-20 20:55:18 +0300

    Expanded simple prime factorization

--- math/number_theory/simple_prime_factorization.cpp
+++ math/number_theory/simple_prime_factorization.cpp
@@ -5,7 +5,7 @@ using namespace std;
 typedef long long ll;
 
 // Sieve of eratosthenes precomputation,
-// O(n log log n)
+// O(n log log n); ~500ms for n = 1e7
 const int MAX = 1e7;
 bitset<MAX+10> is_prime;
 vector<int> primes;
@@ -20,8 +20,9 @@ void sieve() {
     }
 }
 
-// Requires sieve() to be computed before
-// Works for n <= (9999991 * 9999991) = 99999820000081
+// Requires sieve() to be computed before.
+// Works for n < (lp * lp), where pl is
+// the last prime in vector<int> primes.
 // ~ O(sqrt(n)/ln(sqrt(n)))
 vector<int> prime_factorization(ll n) {
     vector<int> factors;
@@ -33,6 +34,25 @@ vector<int> prime_factorization(ll n) {
             factors.push_back(pf);
         }
         pf = primes[++pf_idx];
+    }
+
+    if (n != 1) factors.push_back(n);
+    return factors;
+}
+
+// Doesn't require sieve() to be
+// precomputed, but runs slower.
+// O(sqrt(n))
+vector<int> single_prime_factorization(ll n) {
+    vector<int> factors;
+    ll a = 2;
+
+    while (a * a <= n) {
+        while (n % a == 0) { 
+            n /= a; 
+            factors.push_back(a); 
+        }
+        ++a;
     }
 
     if (n != 1) factors.push_back(n);
