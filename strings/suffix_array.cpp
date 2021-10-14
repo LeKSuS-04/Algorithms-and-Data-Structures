83ced8ee75d0af0a5b1076d51533d9c47ad60e47
LeKSuS-04 <tarasovion2004@gmail.com>
2021-10-14 21:09:37 +0300

    Suffix array (unfinished)

---
+++ strings/suffix_array.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Still under work
+
+const int ASCII_MAX = 260;
+string T;
+int n;
+vector<int> RA, tempRA;
+vector<int> SA, tempSA;
+vector<int> c;
+
+void counting_sort(int k) {
+    int i, sum, maxi = max(ASCII_MAX, n);
+    c.assign(maxi, 0);
+
+    for (i = 0; i < n; i++)
+        c[i + k < n ? RA[i + k] : 0]++;
+
+    for (i = sum = 0; i < maxi; i++) {
+        int t = c[i];
+        c[i] = sum;
+        sum += t;
+    }
+
+    for (i = 0; i < n; i++)
+        tempSA[
+            c[SA[i] + k < n ? RA[SA[i] + k] : 0]++
+            ] = SA[i];
+    SA = tempSA;
+}
+
+void construct_SA() {
+    RA.resize(n), tempRA.resize(n);
+    SA.resize(n), tempSA.resize(n);
+
+    int i, k, r;
+    for (i = 0; i < n; i++) RA[i] = T[i];
+    for (i = 0; i < n; i++) SA[i] = i;
+    for (k = 1; k < n; k <<= 1) {
+        counting_sort(k);
+
+        counting_sort(0);
+        tempRA[SA[0]] = r = 0;
+        for (i = 1; i < n; i++) 
+            tempRA[SA[i]] = (RA[SA[i]] == RA[SA[i - 1]] && RA[SA[i] + k] == RA[SA[i - 1] + k]) ? r : ++r;
+        RA = tempRA;
+        if (RA[SA[n - 1]] == n - 1) break;
+    }
+}
+
+int main() {
+    getline(cin, T);
+    T += '$';
+    n = T.size();
+
+    construct_SA();
+
+    for (int i = 0; i < n; i++) 
+        cout << SA[i] << "\t\"" << T.substr(SA[i]) << "\"\n";
+}
