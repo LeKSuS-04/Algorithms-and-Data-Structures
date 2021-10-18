29bca505a22ebae47c65d59d09c8556ed6008e5e
LeKSuS-04 <tarasovion2004@gmail.com>
2021-10-18 20:32:39 +0300

    LCA (Longest Common Prefix) on Suffix Array

--- strings/suffix_array.cpp
+++ strings/suffix_array.cpp
@@ -9,13 +9,19 @@ using namespace std;
 // of suffix on i-th place among all other
 // suffixes when sorted in lexicographic
 // order
+// SuffixArray.LCP[i] -> amount of similar
+// characters in SuffixArray[i] and 
+// SuffixArray[i - 1]
+
 // SuffixArray.size() -> size of Suffix Array
 // SuffixArray.base_str() -> string from which
 // thi Suffix Array is built. Contains null byte 
 // at the end
+// SuffixArray.update(string s) -> create new 
+// Suffix Array from string s
 
 // OOP style
-class SuffixArray {         
+class SuffixArray {
 private:
     const int ASCII_MAX = 260;
     string T;
@@ -25,8 +31,8 @@ private:
     vector<int> c;
 
     void counting_sort(int k) {
-        c.assign(max(ASCII_MAX, n), 0);
-        for (int i = 0; i < n; i++)
+        c.assign(max(n, ASCII_MAX), 0);
+        for (int i = 0; i < n; i++) 
             c[i + k < n ? RA[i + k] : 0]++;
 
         for (int i = 0, sum = 0; i < c.size(); i++) {
@@ -35,12 +41,12 @@ private:
             sum += t;
         }
 
-        for (int i = 0; i < n; i++) 
+        for (int i = 0; i < n; i++)
             tempSA[c[SA[i] + k < n ? RA[SA[i] + k] : 0]++] = SA[i];
         SA = tempSA;
     }
-public:
-    SuffixArray(string s = "") {
+
+    void construt_SA(string s) {
         T = s + char(0);
         n = T.size();
 
@@ -57,12 +63,23 @@ public:
             RA = tempRA;
             if (RA[SA[n - 1]] == n - 1) break;
         }
-    }
 
+        LCP.resize(n);
+        for (int i = 0, k = 0; i < n - 2; i++) {
+            while (T[i + k] == T[SA[RA[i] - 1] + k]) ++k;
+            LCP[RA[i]] = k;
+            k = max(k - 1, 0);
+        }
+    }
+public:
+    vector<int> LCP;
+    SuffixArray(string s = "")  { construt_SA(s); }
+
+    void update(string s) { construt_SA(s); }
     int operator[](int idx) { return SA[idx]; }
-    string base_str() { return T; }
     int size() { return n; }
 };
+
 
 int main() {    
     string s;
@@ -70,5 +87,5 @@ int main() {
     SuffixArray SA(s);
 
     for (int i = 0; i < SA.size(); i++) 
-        cout << SA[i] << "\t\"" << s.substr(SA[i]) << "\"\n";
+        cout << SA.LCP[i] << " " << SA[i] << "\t\"" << s.substr(SA[i]) << "\"\n";
 }
