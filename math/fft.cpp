7933641ae5132cad12d5f307e7dc0e951f51452a
Alexey Tarasov <tarasovion2004@gmail.com>
2024-05-10 21:12:58 +0000

    :nerd:

--- math/fft.cpp
+++ math/fft.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 constexpr double pi = 3.141592653589793;
-typedef complex<double> compl_d;
+typedef complex<double> domain;
 
-void fft(vector<compl_d>& p, compl_d wn) {
+void fft(vector<domain>& p, domain wn) {
     int n = p.size();
     if (n == 1) {
         return;
     }
 
-    vector<compl_d> even(n/2), odd(n/2);
+    vector<domain> even(n/2), odd(n/2);
     for (int i = 0; i < n/2; i++) {
         even[i] = p[2*i];
         odd[i] = p[2*i + 1];
@@ -19,7 +19,7 @@ void fft(vector<compl_d>& p, compl_d wn) {
     fft(even, wn * wn);
     fft(odd, wn * wn);
 
-    compl_d w = 1;
+    domain w = 1;
     for (int i = 0; i < n/2; i++) {
         p[i] = even[i] + w * odd[i];
         p[i + n/2] = even[i] - w * odd[i];
@@ -31,13 +31,13 @@ bool is_power_2(int value) {
     return (value & (value - 1)) == 0;
 }
 
-vector<compl_d> evaluate(const vector<int>& coefs) {
-    vector<compl_d> points = {coefs.begin(), coefs.end()};
+vector<domain> evaluate(const vector<int>& coefs) {
+    vector<domain> points = {coefs.begin(), coefs.end()};
     fft(points, polar(1.0, 2 * pi / points.size()));
     return points;
 }
 
-vector<int> interpolate(vector<compl_d> points) {
+vector<int> interpolate(vector<domain> points) {
     int n = points.size();
     fft(points, polar(1.0, -2 * pi / points.size()));
     vector<int> coefs;
@@ -56,7 +56,7 @@ vector<int> multiply(vector<int> a, vector<int> b) {
 
     auto points_a = evaluate(a);
     auto points_b = evaluate(b);
-    vector<compl_d> points_c(a.size());
+    vector<domain> points_c(a.size());
 
     for (int i = 0; i < points_c.size(); i++) {
         points_c[i] = points_a[i] * points_b[i];
